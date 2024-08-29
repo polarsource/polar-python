9017446cfc743a5de9e61175a690222244d94145
François Voron <fvoron@gmail.com>
2024-08-29 18:17:56 +0200

    Bump version

--- src/polar/sdkconfiguration.py
+++ src/polar/sdkconfiguration.py
@@ -26,9 +26,9 @@ class SDKConfiguration:
     server_idx: Optional[int] = 0
     language: str = "python"
     openapi_doc_version: str = "0.1.0"
-    sdk_version: str = "0.0.2"
+    sdk_version: str = "0.1.1"
     gen_version: str = "2.404.11"
-    user_agent: str = "speakeasy-sdk/python 0.0.2 2.404.11 0.1.0 polar"
+    user_agent: str = "speakeasy-sdk/python 0.1.1 2.404.11 0.1.0 polar"
     retry_config: OptionalNullable[RetryConfig] = Field(default_factory=lambda: UNSET)
     timeout_ms: Optional[int] = None
 
